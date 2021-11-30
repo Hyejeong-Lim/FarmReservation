48518f5085a6058b2ec2df1f2a7849e6f8b211ad
lhj99 <inklhj99@gmail.com>
2021-11-30 19:13:36 +0900

    로그인 페이지 생성 및 이동

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+/* GET home page. */
+router.get('/login', function (req, res, next) {
+  res.render('login', { title: 'Express' });
 });
 
 module.exports = router;
