54be10da6874f98fa28964039bacd34ed61b8d13
lhj99 <inklhj99@gmail.com>
2021-12-07 15:15:27 +0900

    router추가

--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,16 @@ router.get('/login', function (req, res, next) {
   res.render('login', { title: 'Express' });
 });
 
+router.get('/signup', function(req, res, next) {
+  res.render('signup', { title: 'Express' });
+});
+
+router.get('/farm_reg', function(req, res, next) {
+  res.render('farm_reg', { title: 'Express' });
+});
+
+router.get('/reserve', function(req, res, next) {
+  res.render('reserve', { title: 'Express' });
+});
+
 module.exports = router;
