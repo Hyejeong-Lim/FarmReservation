b15ea3e370916b50003c9596ccfa8eb4623a6212
lhj99 <inklhj99@gmail.com>
2021-12-07 22:22:54 +0900

    users 수정

--- routes/users.js
+++ routes/users.js
@@ -18,4 +18,15 @@ router.get('/reserve', function(req, res, next) {
   res.render('reserve', { title: 'Express' });
 });
 
+router.get('/search_list', function(req, res, next) {
+  res.render('search_list', { title: 'Express' });
+});
+
+router.get('/farmMypage', function(req, res, next) {
+  res.render('farmMypage', { title: 'Express' });
+});
+
+router.get('/farmInfo', function(req, res, next) {
+  res.render('farmInfo', { title: 'Express' });
+});
 module.exports = router;
